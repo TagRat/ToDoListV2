e2765d0e8fe09223350bd7a322782b237dd7d3e8
Tagrat <tagg12352@gmail.com>
2017-07-27 19:05:20 -0400

    first commit - using Ch5 product manager as base code

---
+++ view/header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<!-- the head section -->
+<head>
+    <title>Simple To Do List</title>
+    <link rel="stylesheet" type="text/css"
+          href="../main.css">
+</head>
+
+<!-- the body section -->
+<body>
+<header><h1>Simple To Do List</h1></header>
